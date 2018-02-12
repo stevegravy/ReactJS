db1303625c3845697ae74510d9c18b20b72d13bb
Unknown <contact@stevegravy.com>
2018-02-12 17:09:17 +0100

    markdown 2.0

--- markdown/src/index.js
+++ markdown/src/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+//CSS
+import './style/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//JS importé
+import {sampleText} from './sampleText';
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+class App extends React.Component {
+
+    state = {
+        text:sampleText
+    };
+
+    editText=(event) => {
+        const text = event.target.value;
+        this.setState({ text });
+    }
+
+    render(){
+        return(
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <textarea className="form-control" value={this.state.text} id="" cols="30" rows="35" onchange={(e) => this.editText(e)}>
+
+                        </textarea>
+                    </div>
+                    <div className="col-sm-6">
+                        {this.state.text}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+render(
+  <App/>,
+  document.getElementById('root')
 );
-registerServiceWorker();
