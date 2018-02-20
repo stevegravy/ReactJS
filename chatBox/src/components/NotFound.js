190c8c24f2a213c72a7a89ff91560a49a8f18742
user <contact@stevegravy.com>
2018-02-20 16:28:55 +0100

    chatbox 2.0

---
+++ chatBox/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+        <h2 className="notfound">This page is not found</h2>
+    )
+}
+
+export default NotFound;
--- chatBox/src/index.js
+++ chatBox/src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+//import Connexion from './components/Connexion';
+import App from './components/App';
+import Connexion from './components/Connexion';
+import NotFound from './components/NotFound';
+//router
+import {BrowserRouter, Match, Miss} from 'react-router';
 
-class Connexion extends React.Component{
-    render(){
-        return(
-            <div className="connectionBox">
-                <form className="connexion">
-                    <input type="text" placeholder="Pseudo" required/>
-                    <button type="submit">GO</button>
-                </form>
+const Root = () => {
+    return(
+        <BrowserRouter>
+            <div>
+                <Match exactly pattern="/" component={Connexion} />
+                <Match pattern="/pseudo/:pseudo" component={App} />
+                <Miss component={NotFound} />
             </div>
-        )
-    }
+        </BrowserRouter>
+    )
 }
 
-ReactDOM.render(<Connexion/>, document.getElementById('root'));
+ReactDOM.render(<Root/>, document.getElementById('root'));
